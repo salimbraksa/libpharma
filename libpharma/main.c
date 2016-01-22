fe441364c0dc87b595c0485856ff0df950d2b68a
salimbraksa <salim.braksa@gmail.com>
2016-01-22 20:05:13 +0000

    Initial Commit

---
+++ libpharma/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  libpharma
+//
+//  Created by Salim Braksa on 1/22/16.
+//  Copyright © 2016 Braksa Salim. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
